7904e9d96c22848c046a468a4dd2ab974e773d40
rewqqx <rewqqx@gmail.com>
2023-03-10 17:47:10 +0300

    djvu upd

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from htmlparser import HTMLparser, pdf2html, doc2html, djvu2txt
 from files import txt2json
 
 if __name__ == "__main__":
-    # HTMLparser(pdf2html('../files', 'sample')).save_to_json('sample', '../files')
-    # HTMLparser(doc2html('../files', 'test')).save_to_json('test', '../files')
+    HTMLparser(pdf2html('../files', 'sample')).save_to_json('sample', '../files')
+    HTMLparser(doc2html('../files', 'test')).save_to_json('test', '../files')
     txt2json(djvu2txt('E:\Projects\\test\\files', 'sample1'), 'E:\Projects\\test\\files', 'sample1')
